fac3585e2cede61ddf3dc37264b5376125303eb6
Nibir <nibirpthk@gmail.com>
2022-11-18 12:08:35 -0600

    Add files via upload

---
+++ Unet_focal_loss.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Focal loss model
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Conv2DTranspose, BatchNormalization, Dropout, Lambda
+from keras import backend as K
+
+def focal_loss(gamma=5.5, alpha=4.):
+
+    gamma = float(gamma)
+    alpha = float(alpha)
+
+    def focal_loss_fixed(y_true, y_pred):
+        epsilon = 1.e-9
+        y_true = tf.convert_to_tensor(y_true, tf.float32)
+        y_pred = tf.convert_to_tensor(y_pred, tf.float32)
+
+        yp = tf.add(y_pred, epsilon)
+        loss1 = tf.multiply(y_true, -tf.math.log(yp))
+        loss2 = tf.multiply(loss1, tf.pow(tf.subtract(1., yp), gamma))
+        return loss2
+    return focal_loss_fixed
+
+def multi_unet_model(n_classes=3, IMG_HEIGHT=128, IMG_WIDTH=128, IMG_CHANNELS=1):
+
+#Build the model
+    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+    #s = Lambda(lambda x: x / 255)(inputs)   #No need for this if we normalize our inputs beforehand
+    s = inputs
+
+    #Contraction path
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+    
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+     
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+     
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+     
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    
+    #Expansive path 
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+     
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+     
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+     
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+     
+    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
+     
+    model = Model(inputs=[inputs], outputs=[outputs])
+    
+    #NOTE: Compile the model in the main program to make it easy to test with various loss functions
+    #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer = 'adam', loss = focal_loss(alpha=1), metrics = ['accuracy'])
+    #model.summary()
+    
+    return model
+
+
+# In[2]:
+
+
+from keras.utils.np_utils import normalize
+#import os
+#import glob
+#import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import tifffile as tiff
+from patchify import patchify, unpatchify
+import tensorflow as tf
+
+
+# In[3]:
+
+
+#Resizing images, if needed
+SIZE_X = 696 
+SIZE_Y = 520
+n_classes= 3 #Number of classes for segmentation
+
+
+# In[4]:
+
+
+train_images = tiff.imread('D:/Machine Learning/Unet_data/Cell/BBBC/train_img.tif')
+train_masks = tiff.imread('D:/Machine Learning/Unet_data/Cell/BBBC/Mask_BBBC.tif')
+
+
+# In[5]:
+
+
+#Convert list to array for machine learning processing        
+train_images = np.array(train_images)
+train_masks = np.array(train_masks)
+
+R=(train_masks[:,:,:,0]/255)*1
+G=(train_masks[:,:,:,1]/255)*2
+B=(train_masks[:,:,:,2]/255)*3
+train_masks=R+G+B
+train_masks = np.uint8(train_masks)
+
+
+# In[6]:
+
+
+#Encode labels... but multi dim array so need to flatten, encode and reshape
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+n, h, w = train_masks.shape
+train_masks_reshaped = train_masks.reshape(-1,1)
+train_masks_reshaped_encoded = labelencoder.fit_transform(train_masks_reshaped)
+train_masks_encoded_original_shape = train_masks_reshaped_encoded.reshape(n, h, w)
+train_masks_encoded_original_shape = np.uint8(train_masks_encoded_original_shape)
+
+
+# In[7]:
+
+
+all_img_patches = []
+for img in range(train_images.shape[0]):
+    #print(img)     #just stop here to see all file names printed
+     
+    large_image = train_images[img]
+    
+    patches_img = patchify(large_image, (128, 128), step=128)  #Step=256 for 256 patches means no overlap, get 12 patches from a single large img
+    
+
+    for i in range(patches_img.shape[0]):
+        for j in range(patches_img.shape[1]):
+            
+            single_patch_img = patches_img[i,j,:,:]
+            single_patch_img = (single_patch_img.astype('float32')) / 255.
+            #scaler = MinMaxScaler()
+            #single_patch_img= scaler.fit_transform(single_patch_img)
+            
+            all_img_patches.append(single_patch_img)
+
+images = np.array(all_img_patches)
+train_images = normalize(images, axis=1)
+train_images = np.expand_dims(train_images, axis=3)
+
+
+# In[8]:
+
+
+all_mask_patches = []
+for img in range(train_masks_encoded_original_shape.shape[0]):
+    #print(img)     #just stop here to see all file names printed
+     
+    large_mask = train_masks_encoded_original_shape[img]
+    
+    patches_mask = patchify(large_mask, (128, 128), step=128)  #Step=256 for 256 patches means no overlap
+    
+
+    for i in range(patches_mask.shape[0]):
+        for j in range(patches_mask.shape[1]):
+            
+            single_patch_mask = patches_mask[i,j,:,:]
+            #single_patch_mask = single_patch_mask / 255.
+            
+            all_mask_patches.append(single_patch_mask)
+
+masks = np.array(all_mask_patches)
+train_masks_input = np.expand_dims(masks, axis=3)
+
+
+# In[9]:
+
+
+from sklearn.model_selection import train_test_split
+X1, X_test, y1, y_test = train_test_split(train_images, train_masks_input, test_size = 0.10, random_state = 0)
+
+#Further split training data t a smaller subset for quick testing of models
+X_train, X_do_not_use, y_train, y_do_not_use = train_test_split(X1, y1, test_size = 0.2, random_state = 0)
+
+
+print("Class values in the dataset are ... ", np.unique(y_train))  # 0 is the background/few unlabeled 
+
+from keras.utils.np_utils import to_categorical
+train_masks_cat = to_categorical(y_train, num_classes=n_classes)
+y_train_cat = train_masks_cat
+
+test_masks_cat = to_categorical(y_test, num_classes=n_classes)
+y_test_cat = test_masks_cat
+
+
+# In[10]:
+
+
+IMG_HEIGHT = X_train.shape[1]
+IMG_WIDTH  = X_train.shape[2]
+IMG_CHANNELS = X_train.shape[3]
+print(IMG_HEIGHT)
+print(IMG_WIDTH)
+print(IMG_CHANNELS)
+
+
+# In[11]:
+
+
+model = multi_unet_model()
+
+
+# In[12]:
+
+
+
+history = model.fit(X_train, y_train_cat, batch_size = 16, verbose=1, epochs=1, validation_data=(X_test, y_test_cat),
+                    #class_weight=class_weights,
+                    shuffle=False)
+
+
+# In[14]:
+
+
+_, acc = model.evaluate(X_test, y_test_cat)
+print("Accuracy is = ", (acc * 100.0), "%")
+
+
+# In[15]:
+
+
+y_pred=model.predict(X_test)
+y_pred_argmax=np.argmax(y_pred, axis=3)
+
+
+# In[16]:
+
+
+#Using built in keras function
+from keras.metrics import MeanIoU
+n_classes =3
+IOU_keras = MeanIoU(num_classes=n_classes)  
+IOU_keras.update_state(y_test[:,:,:,0], y_pred_argmax)
+print("Mean IoU =", IOU_keras.result().numpy())
+
+
+# In[17]:
+
+
+#To calculate I0U for each class...
+values = np.array(IOU_keras.get_weights()).reshape(n_classes, n_classes)
+print(values)
+class1_IoU = values[0,0]/(values[0,0] + values[0,1] + values[0,2] + values[1,0]+ values[2,0])
+class2_IoU = values[1,1]/(values[1,1] + values[1,0] + values[1,2] + values[0,1]+ values[2,1])
+class3_IoU = values[2,2]/(values[2,2] + values[2,0] + values[2,1] + values[0,2]+ values[1,2])
+
+
+print("IoU for class1 is: ", class1_IoU)
+print("IoU for class2 is: ", class2_IoU)
+print("IoU for class3 is: ", class3_IoU)
+
+
+# In[18]:
+
+
+
+plt.imshow(train_images[0, :,:,0], cmap='gray')
+plt.imshow(train_masks[0], cmap='gray')
+
+import random
+test_img_number = random.randint(0, len(X_test))
+test_img = X_test[test_img_number]
+ground_truth=y_test[test_img_number]
+test_img_norm=test_img[:,:,0][:,:,None]
+test_img_input=np.expand_dims(test_img_norm, 0)
+prediction = (model.predict(test_img_input))
+predicted_img=np.argmax(prediction, axis=3)[0,:,:]
+
+
+# In[19]:
+
+
+plt.figure(figsize=(12, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_img[:,:,0], cmap='gray')
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(ground_truth[:,:,0], cmap='jet')
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(predicted_img, cmap='jet')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
